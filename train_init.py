a1e618e666b3d8722921ef6eb2d6ed743c2d50d9
Ksemchh <ksemchh@vk.com>
2024-05-29 17:00:23 +0500

    add predict for directions

--- train_init.py
+++ train_init.py
@@ -8,13 +8,14 @@ from keras.callbacks import ModelCheckpoint
 from keras import regularizers
 from keras import metrics
 from keras.utils import plot_model
-from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error, accuracy_score
 import matplotlib.pyplot as plt
 import psycopg2
 from DataBase_config import host, user, password, db_name
 from Links import stocks
 from tqdm import tqdm
 import os
+pd.set_option('display.max_columns', None)
 
 connection = psycopg2.connect(
         host=host,
@@ -33,14 +34,14 @@ def createDataset(dataset, target, lookBack=1):
 
 # основная программа
 abbr_flag = True
-#abbr = 'afks'
-#for i in range(1):
-for abbr, link in tqdm(stocks.items(), ncols=100):
-    if abbr_flag:
-        if abbr == 'qiwidr':
-            ...
-        else:
-            continue
+abbr = 'alrs'
+for i in range(1):
+# for abbr, link in tqdm(stocks.items(), ncols=100):
+#     if abbr_flag:
+#         if abbr == 'qiwidr':
+#             ...
+#         else:
+#             continue
     # os.mkdir(f'Models/{abbr}')
     df = pd.read_sql_query(f'''SELECT date, price, open, neutral, positive, negative
                           FROM {abbr}_feat;''',connection)
@@ -117,12 +118,23 @@ for abbr, link in tqdm(stocks.items(), ncols=100):
     dfTrainPredictInv['date'] = df['date'][lookBack:len(trainPredictInv) + lookBack].values
     dfTrain = pd.merge(df, dfTrainPredictInv, how='left', on='date')
     dfTrain = dfTrain.dropna(subset='predict')
+    dfTrain['change'] = dfTrain['price'].rolling(2, min_periods=2).apply(lambda x: (x.iat[-1] / x.iat[-2]) * 100 - 100)
+    dfTrain['direction'] = dfTrain['change'] > 0
+    dfTrain['predictChange'] = dfTrain['predict'].rolling(2, min_periods=2).apply(lambda x: (x.iat[-1] / x.iat[-2]) * 100 - 100)
+    dfTrain['predictDirection'] = dfTrain['predictChange'] > 0
+    print(dfTrain.head(30))
+
 
     testPredictInv1 = list(map(lambda x: float(*x), testPredictInv))
     dfTestPredictInv = pd.DataFrame(testPredictInv1, columns=['predict'])
     dfTestPredictInv['date'] = df['date'][len(trainPredictInv) + lookBack:].values
     dfTest = pd.merge(df, dfTestPredictInv, how='left', on='date')
     dfTest = dfTest.dropna(subset='predict')
+    dfTest['change'] = dfTest['price'].rolling(2, min_periods=2).apply(lambda x: (x.iat[-1] / x.iat[-2]) * 100 - 100)
+    dfTest['direction'] = dfTest['change'] > 0
+    dfTest['predictChange'] = dfTest['predict'].rolling(2, min_periods=2).apply(lambda x: (x.iat[-1] / x.iat[-2]) * 100 - 100)
+    dfTest['predictDirection'] = dfTest['predictChange'] > 0
+    print(dfTest.head(30))
 
     trainMSE = mean_squared_error(dfTrain['price'], dfTrain['predict'])
     print(f'trainMSE: {trainMSE}')
@@ -144,29 +156,34 @@ for abbr, link in tqdm(stocks.items(), ncols=100):
     testMAPE = mean_absolute_percentage_error(dfTest['price'], dfTest['predict'])
     print(f'testMAPE: {testMAPE}')
 
-    with open(f'Models/{abbr}/model_description.txt', mode='w', encoding='utf-8') as file:
-        print('Model Summary:', file=file)
-        print(model.summary(), file=file)
-        print('Predict table:', file=file)
-        print(dfTrain.head(), file=file)
-        print(f'Training MSE: {trainMSE}', file=file)
-        print(f'Testing MSE: {testMSE}', file=file)
-        print(f'Training RMSE: {trainRMSE}', file=file)
-        print(f'Testing RMSE: {testRMSE}', file=file)
-        print(f'Training MAE: {trainMAE}', file=file)
-        print(f'Testing MAE: {testMAE}', file=file)
-        print(f'Training MAPE: {trainMAPE}', file=file)
-        print(f'Testing MAPE: {testMAPE}', file=file)
-
-    dates = df['date'].values[lookBack:]
-    sampleInterval = 50
-    sampledDates = dates[::sampleInterval]
-
-    plt.figure(figsize=(20, 10))
-    plt.plot(dfTrain['date'], dfTrain['price'], label='Actual Train')
-    plt.plot(dfTrain['date'], dfTrain['predict'], label='Predicted Train')
-    plt.plot(dfTest['date'], dfTest['price'], label='Actual Test')
-    plt.plot(dfTest['date'], dfTest['predict'], label='Predicted Test')
-    plt.xticks(sampledDates, rotation=90)
-    plt.legend()
-    plt.savefig(f'Models/{abbr}/Train result plot', dpi=500, bbox_inches='tight')
+    trainAccuracy = accuracy_score(dfTrain['direction'], dfTrain['predictDirection'])
+    print(f'trainAccuracy: {trainAccuracy}')
+    testAccuracy = accuracy_score(dfTest['direction'], dfTest['predictDirection'])
+    print(f'testAccuracy: {testAccuracy}')
+    #
+    # with open(f'Models/{abbr}/model_description.txt', mode='w', encoding='utf-8') as file:
+    #     print('Model Summary:', file=file)
+    #     print(model.summary(), file=file)
+    #     print('Predict table:', file=file)
+    #     print(dfTrain.head(), file=file)
+    #     print(f'Training MSE: {trainMSE}', file=file)
+    #     print(f'Testing MSE: {testMSE}', file=file)
+    #     print(f'Training RMSE: {trainRMSE}', file=file)
+    #     print(f'Testing RMSE: {testRMSE}', file=file)
+    #     print(f'Training MAE: {trainMAE}', file=file)
+    #     print(f'Testing MAE: {testMAE}', file=file)
+    #     print(f'Training MAPE: {trainMAPE}', file=file)
+    #     print(f'Testing MAPE: {testMAPE}', file=file)
+    #
+    # dates = df['date'].values[lookBack:]
+    # sampleInterval = 50
+    # sampledDates = dates[::sampleInterval]
+    #
+    # plt.figure(figsize=(20, 10))
+    # plt.plot(dfTrain['date'], dfTrain['price'], label='Actual Train')
+    # plt.plot(dfTrain['date'], dfTrain['predict'], label='Predicted Train')
+    # plt.plot(dfTest['date'], dfTest['price'], label='Actual Test')
+    # plt.plot(dfTest['date'], dfTest['predict'], label='Predicted Test')
+    # plt.xticks(sampledDates, rotation=90)
+    # plt.legend()
+    # plt.savefig(f'Models/{abbr}/Train result plot', dpi=500, bbox_inches='tight')
